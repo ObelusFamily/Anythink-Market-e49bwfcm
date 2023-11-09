d33c901c6a6e1c8940be04cb29ebba1e637c2797
xavier Bélénus <xavier.belenus@gmail.com>
2023-11-09 13:52:14 +0000

    seeding bis

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -7,18 +7,20 @@ var User = mongoose.model("User");
 var Comment = mongoose.model("Comment");
 var Item = mongoose.model("Item");
 
-
-for(let i = 0; i < 100; i++){
-    let user = new User();
-  user.username = "woooz"+i;
-  user.email = i+"zoooow.beooos@gmzal.com";
-  user.setPassword("password")
-  user.save()
-    let item = new Item()
-    item.title = "title"+i
-    item.description = "description"+i
-    item.save()
-    let comment = new Comment();
-    comment.body = "comment"+i
-    comment.save()
+function seed(){
+    for(let i = 0; i < 100; i++){
+        let user = new User();
+      user.username = "plpk"+i;
+      user.email = i+"qddf.beooos@gmaezal.com";
+      user.setPassword("password")
+      user.save()
+        let item = new Item()
+        item.title = "aze"+i
+        item.description = "descrezezaiption"+i
+        item.save()
+        let comment = new Comment();
+        comment.body = "commsfsent"+i
+        comment.save()
+    }
 }
+seed()
